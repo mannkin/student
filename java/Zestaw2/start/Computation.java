7dcd520b31676cfd91d5825a2fc8bf105804235c
mannkin <haverlord@gmail.com>
2018-11-07 09:32:50 +0000

    Update java/Zestaw2/start/Computation.java
    computation

--- java/Zestaw2/start/Computation.java
+++ java/Zestaw2/start/Computation.java
@@ -3,24 +3,51 @@ public interface Computation<T> {
     T compute(T);
 }
 
-
-
 public class Multiplier implements Computation<Double> {
-
 	private double a;
 	
-	public Multiplier(double x) {
-		a = x;
+	public Multiplier(double a) {
+		this.a = x;
 	}
 
-	@Override
 	double compute(double x) {
+		return a*x;
+	}
+
+	double static computeStatic(double x) {
 		return a*x;
 	}
 }
 
 public class Compute {
-	public static void main(String[] args) {}
-	public static void plot(double xmin, double xmax, Computation<Double> f) {}
-	public static void apply(double[] array, Computation<Double> f) {}
+	public static void main(String[] args) {
+		Multiplier mul = new Multiplier(2);
+
+		plot(3, 10, mul);
+		plot(3, 10, new Computation<Double>() {
+			@Override
+			public double compute(double x) {
+				return a*x;
+			}
+		});
+		Computation<Double> comp = x->x*a;
+		plot(3, 10, comp);
+		plot(3, 10, Multiplier::compute);
+		plot(3, 10, Multiplier::computeStatic);
+	}
+
+	public static void plot(double xmin, double xmax, Computation<Double> f) {
+		final int n = 20;
+		int counter = Math.abs(xmax-xmin);
+
+		for(int i = xmin; i <= xmax; i++) {
+			System.out.println(i+"  "+counter+"  "+f.compute(i));
+		}
+	}
+
+	public static void apply(double[] array, Computation<Double> f) {
+		for(int i = 0; i < array.length; i++) {
+			array[i] = f.compute(array[i]);
+		}
+	}
 }
